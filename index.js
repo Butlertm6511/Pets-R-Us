a10dfa5fe9d798ace0d5e1e728afbe16e1c99e0d
TiaNiecia Mosley Butler <Butlertm1165@gmail.com>
2023-05-13 01:19:36 -0600

    5.2

--- index.js
+++ index.js
@@ -4,47 +4,51 @@
   Description:Set up dependencies, nodemon, express. Creates system for setting up node project, writing express server,
   use of templates, using routes, and connecting static folders.*/
   
-
+ 
 
 const express = require('express');
 const app = express();
 const path = require('path');
+// const expresslayouts = require('express-ejs-layouts')
 
 
 
 // statics files
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 
-
+// app.use(expresslayouts)
 app.use(express.static(path.join(__dirname,'public')));
 
 
 
 // Route for the index page and grooming page
 app.get('/', (req, res) => {
-  res.render('index'), {
-    title:'pet-r-us:Landing Page',
-    pageTitle:'Landing Page'
-  }
-}) 
+  res.render('index')
+}); 
 
 
 app.get('/grooming', (req, res) => {
-  res.render('grooming',{
-  title:'pets-r-us:grooming',
-  pageTitle:'grooming'
-  })
-})
+  res.render('grooming');
+});
 
 
+app.get('/boarding', (req, res) => {
+  res.render('boarding');
+});
+
+
+app.get('/training', (req, res) => {
+  res.render('training');
+});
 
 
 
 const Port = process.env.PORT || 3002;
 app.listen(Port, () => {
-  console.log('Server listening on port 3001');
+  console.log('Server listening on port 3002');
 });
 
 
